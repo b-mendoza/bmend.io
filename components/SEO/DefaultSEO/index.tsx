16abfbde6503e1b83cf2c40c898d29dc9546e008
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-12-27 22:23:00 -0600

    feat: add `DefaultSEO` component

---
+++ components/SEO/DefaultSEO/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export function DefaultSEO() {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+  );
+}
