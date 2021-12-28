b404af239b48ce35eef28b642a0f20aa3b3516d1
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-12-27 22:52:26 -0600

    feat: add `_document` component

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class __Document extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default __Document;
