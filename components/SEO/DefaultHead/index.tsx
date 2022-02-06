a7d8d0d2fb676b3f9611d17eb5a06bb7f985813d
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-06 07:10:59 -0600

    feat: add `meta:desc` to `DefaultHead` component

--- components/SEO/DefaultHead/index.tsx
+++ components/SEO/DefaultHead/index.tsx
@@ -5,6 +5,10 @@ export function DefaultHead(): JSX.Element {
     <Head>
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="description"
+        content="Hello! This is Bryan, and I hope you are doing well"
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
   );
