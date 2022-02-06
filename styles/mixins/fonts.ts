2265eef944ef9052d248763c6fbefd8b721a130e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-06 15:19:15 -0600

    feat: improve fallback fonts for `Fira Code`

--- styles/mixins/fonts.ts
+++ styles/mixins/fonts.ts
@@ -14,11 +14,13 @@ export const injectOpenSansFont = css`
 `;
 
 export const injectFiraCodeFont = css`
-  font-family: 'Fira Code', 'Courier New', Courier, monospace;
+  font-family: 'Fira Code', source-code-pro, Menlo, Monaco, Consolas,
+    'Courier New', monospace;
   font-weight: 400;
 
   @supports (font-variation-settings: normal) {
-    font-family: 'Fira CodeVariable', 'Courier New', Courier, monospace;
+    font-family: 'Fira CodeVariable', source-code-pro, Menlo, Monaco, Consolas,
+      'Courier New', monospace;
     font-variation-settings: 'wght' 400;
   }
 `;
