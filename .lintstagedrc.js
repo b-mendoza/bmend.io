ea7f23eebca7103ed69dcf2b10d4d857416f113c
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-16 00:12:08 -0600

    revert: change linter step to use `next lint`

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,15 @@
 // @ts-check
 
+const path = require('path');
+
+/** @param {string[]} filenames */
+
+const buildESLintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((filename) => path.relative(process.cwd(), filename))
+    .join(' --file ')}`;
+
 module.exports = {
   '*': 'prettier -c --config ./.prettierrc -w',
-  '**/*.(j|t)s?(x)': 'eslint',
+  '**/*.(j|t)s?(x)': [buildESLintCommand],
 };
