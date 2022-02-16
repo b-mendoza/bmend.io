fd96e6285d1ea2f4122c53a73a72b589ba1f6bb3
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-16 00:11:01 -0600

    feat: add `@ts-check` to Next.js config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+// @ts-check
+
 const { withPlaiceholder } = require('@plaiceholder/next');
 
 /** @type {import('next').NextConfig} */
