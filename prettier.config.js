1bfb7166c76cd8ef5312ac6e50c52810ba2a55fa
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-11-24 17:39:20 -0600

    chore: add Prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  singleQuote: true,
+  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-tailwindcss'],
+};
+
+export default prettierConfig;
+  
