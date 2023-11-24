ebfe5513e3d63dd8f3271e8884824f8b2d7a8d9b
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-11-24 17:42:59 -0600

    chore: update Remix route convention

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { unstable_vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { createRoutesFromFolders } from '@remix-run/v1-route-convention';
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      routes: (defineRoutes) => {
+        return Promise.resolve(createRoutesFromFolders(defineRoutes));
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
