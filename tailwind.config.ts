0f2768eb5abec4f0cb8fc5aec8ec7e98d5127e8c
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-04 06:58:54 -0600

    feat(styles): integrate Tailwind CSS for styling
    This commit includes the integration of Tailwind CSS into the application. Changes include importing `LinksFunction` from `@remix-run/node`, adding a new link with Tailwind stylesheet to the Root component, and initializing Tailwind CSS configuration.
    
    - The `LinksFunction` is used to link the Tailwind CSS file in the root.tsx.
    - A new CSS file for Tailwind (`tailwind.css`) is added which imports base, components, and utilities from Tailwind.
    - The Tailwind configuration file (`tailwind.config.ts`) has been simplified by removing custom colors, fonts, animations, and other extensions that are no longer necessary. It now exports a minimal configuration and ensures the object satisfies the Tailwind Config type.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,9 @@
 import type { Config } from 'tailwindcss';
 
-const config: Config = {
+export default {
   content: ['./app/**/*.{ts,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
-        },
-      },
-      fontFamily: {
-        lusitana: ['var(--font-lusitana)'],
-        sans: ['var(--font-geist-sans)'],
-      },
-      gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
-      },
-    },
-    keyframes: {
-      shimmer: {
-        '100%': {
-          transform: 'translateX(100%)',
-        },
-      },
-    },
+    extend: {},
   },
-};
-
-export default config;
+  plugins: [],
+} satisfies Config;
