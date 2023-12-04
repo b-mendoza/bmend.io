bb9640ab01c385d03abaaaf724006861fd338e2b
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-04 06:36:39 -0600

    refactor(tailwind-config): remove forms plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,22 @@
 import type { Config } from 'tailwindcss';
-import formsPlugin from '@tailwindcss/forms';
 
 const config: Config = {
   content: ['./app/**/*.{ts,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        lusitana: ['var(--font-lusitana)'],
-        sans: ['var(--font-geist-sans)'],
-      },
-      gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
-      },
       colors: {
         blue: {
           400: '#2589FE',
           500: '#0070F3',
           600: '#2F6FEB',
         },
+      },
+      fontFamily: {
+        lusitana: ['var(--font-lusitana)'],
+        sans: ['var(--font-geist-sans)'],
+      },
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
       },
     },
     keyframes: {
@@ -28,7 +27,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [formsPlugin],
 };
 
 export default config;
