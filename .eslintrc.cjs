ccb1193980249db97b38b0778083ce9234f4ea66
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-04 06:35:40 -0600

    chore(eslint-config): update ESLint rules and environment configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,63 @@
 // @ts-check
 
+// ESLint severity types
+const SeverityTypes = Object.freeze({
+  OFF: 0,
+  WARN: 1,
+  ERROR: 2,
+});
+
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: [
-    'xo',
-    'xo-react',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'standard-with-typescript',
+    'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/strict',
-    'xo-typescript',
     'plugin:deprecation/recommended',
     'prettier',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
     project: ['./tsconfig.json', './tsconfig.eslint.json'],
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
-
-  // ===========================================
-  // Remix specific settings
-  env: {
-    browser: true,
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    '@typescript-eslint/consistent-type-definitions': [
+      SeverityTypes.ERROR,
+      'type',
+    ],
+    '@typescript-eslint/explicit-function-return-type': SeverityTypes.OFF,
+    // '@typescript-eslint/no-namespace': [
+    //   SeverityTypes.ERROR,
+    //   { allowDeclarations: true },
+    // ],
+    'jsx-a11y/anchor-has-content': [
+      SeverityTypes.ERROR,
+      {
+        components: ['Link', 'NavLink'],
+      },
+    ],
+    'react/jsx-no-leaked-render': [
+      SeverityTypes.ERROR,
+      { validStrategies: ['ternary'] },
+    ],
+    'react/prefer-read-only-props': SeverityTypes.ERROR,
   },
   settings: {
     react: {
@@ -30,14 +69,4 @@ module.exports = {
       { name: 'NavLink', linkAttribute: 'to' },
     ],
   },
-  rules: {
-    'jsx-a11y/anchor-has-content': [
-      'error',
-      {
-        components: ['Link', 'NavLink'],
-      },
-    ],
-    'react/jsx-no-leaked-render': ['warn', { validStrategies: ['ternary'] }],
-  },
-  // ===========================================
 };
