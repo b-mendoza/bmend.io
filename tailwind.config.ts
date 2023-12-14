c6f8dc923149a8831350adb2ddf4deba8654339d
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-14 17:52:10 -0600

    feat(theme): integrate HSL color variables and extend Tailwind configuration
    This commit introduces a new set of CSS variables for colors defined in HSL format within the application's global styles. These variables are then used to extend the color theme in the Tailwind CSS configuration, allowing for consistent color theming across the application with adjustable alpha values for transparency control. This enhancement could improve maintainability and provide a more cohesive look and feel across the UI.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,33 @@
 import defaultThemeOpts from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 
+const ALPHA_VALUE = '<alpha-value>';
+
 export default {
   content: ['./app/**/*.{ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+
+        // Custom CSS vars
+        background: `hsl(var(--background) / ${ALPHA_VALUE})`,
+        'block-background-1': `hsl(var(--block-background-1) / ${ALPHA_VALUE})`,
+        'block-background-2': `hsl(var(--block-background-2) / ${ALPHA_VALUE})`,
+        'block-border': `hsl(var(--block-border) / ${ALPHA_VALUE})`,
+        'button-hover-background': `hsl(var(--button-hover-background) / ${ALPHA_VALUE})`,
+        'button-hover-text': `hsl(var(--button-hover-text) / ${ALPHA_VALUE})`,
+        'nav-link-background-hover': `hsl(var(--nav-link-background-hover) / ${ALPHA_VALUE})`,
+        'nav-link-background': `hsl(var(--nav-link-background) / ${ALPHA_VALUE})`,
+        'nav-link-text-hover': `hsl(var(--nav-link-text-hover) / ${ALPHA_VALUE})`,
+        'section-background-bottom': `hsl(var(--section-background-bottom) / ${ALPHA_VALUE})`,
+        'section-background-top': `hsl(var(--section-background-top) / ${ALPHA_VALUE})`,
+        'section-border': `hsl(var(--section-border) / ${ALPHA_VALUE})`,
+        texts: `hsl(var(--texts) / ${ALPHA_VALUE})`,
+        'violet-glow': `hsl(var(--violet-glow) / ${ALPHA_VALUE})`,
+        white: `hsl(var(--white) / ${ALPHA_VALUE})`,
+      },
       fontFamily: {
         sans: ['Geist', ...defaultThemeOpts.fontFamily.sans],
       },
