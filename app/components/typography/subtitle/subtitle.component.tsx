97602ff3c3c6c451b647744e9763644aeaab28c9
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-14 23:42:54 -0600

    feat(typography): add subtitle component
    This commit introduces a new `Subtitle` component in the typography system, leveraging existing paragraph styles and utilities to ensure consistency and extendability within the UI components library. The `Subtitle` component is a styled wrapper around the `Paragraph` component, adding boldness and uppercase styling for typographic hierarchy.

---
+++ app/components/typography/subtitle/subtitle.component.tsx
@@ -0,0 +1,16 @@
+import type { ParagraphProps } from '~/components/typography/paragraph';
+import { Paragraph } from '~/components/typography/paragraph';
+import { cn } from '~/utils/cn';
+
+type SubtitleProps = ParagraphProps;
+
+export const Subtitle = (props: SubtitleProps) => {
+  const { className, ...restOfProps } = props;
+
+  return (
+    <Paragraph
+      className={cn('font-semibold uppercase text-texts', className)}
+      {...restOfProps}
+    />
+  );
+};
