48341d9bfe2325e34c6394b21755de3452064c6f
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-14 23:47:58 -0600

    feat(tags): add server utility to fetch static list of tags
    Add a new utility function for generating a predefined list of tags with unique identifiers. This implementation currently uses in-memory generation but is marked for potential future replacement by a database or CMS integration. The `getTags` function creates tags relevant to design, such as 'UI/UX Design', 'Brand Identity', and others, each with a unique ID provided by `crypto.randomUUID()`.

---
+++ app/routes/_index/get-tags.server.ts
@@ -0,0 +1,33 @@
+export const getTags = () => {
+  // This might be replaced by a database/CMS call in the future
+  return [
+    {
+      id: crypto.randomUUID(),
+      text: 'UI/UX Design',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Interaction Design',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Brand Identity',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Web Design',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Product Design',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Typography',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'App User Interface',
+    },
+  ];
+};
