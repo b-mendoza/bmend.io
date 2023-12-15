0aa70aeed8a8aa7876333829c5581f70c1253781
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-14 23:41:40 -0600

    feat(tag-mapper): add new TagMapper component
    Add a new `TagMapper` component to map through an array of tags and render them as a list, utilizing generic typing for flexibility with different tag data structures. This includes the core component file as well as an index file to facilitate exports from the `tag-mapper` directory. The component accepts custom class names for styling individual tags and the wrapper element.

---
+++ app/components/tag-mapper/tag-mapper.component.tsx
@@ -0,0 +1,27 @@
+import { cn } from '~/utils/cn';
+import { Tag } from '~/components/tag';
+
+type TagMapperProps<T> = Readonly<{
+  getTagId: (tag: T) => string;
+  getTagName: (tag: T) => string;
+  tags: T[];
+  tagClassName?: string;
+  tagsWrapperClassName?: string;
+}>;
+
+export const TagMapper = <Tag,>(props: TagMapperProps<Tag>) => {
+  const { getTagId, getTagName, tags, tagClassName, tagsWrapperClassName } =
+    props;
+
+  return (
+    <ul className={cn('flex flex-wrap gap-[0.8rem]', tagsWrapperClassName)}>
+      {tags.map((tag) => (
+        <Tag
+          className={tagClassName}
+          key={getTagId(tag)}
+          name={getTagName(tag)}
+        />
+      ))}
+    </ul>
+  );
+};
