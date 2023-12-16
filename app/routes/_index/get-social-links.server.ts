ed7eb99155f7402818a73b4943346e5d7f17e547
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-16 01:59:37 -0600

    feat(server): add server utility to retrieve social links
    Add a new server-side function `getSocialLinks` that returns an array of social media links with unique IDs. The function utilizes the crypto module to generate a random UUID for each link, ensuring they have a unique identifier. This is useful for rendering a list of social links on the frontend where a key might be required.

---
+++ app/routes/_index/get-social-links.server.ts
@@ -0,0 +1,21 @@
+import crypto from 'crypto';
+
+export const getSocialLinks = () => {
+  return [
+    {
+      id: crypto.randomUUID(),
+      name: 'LinkedIn',
+      to: 'https://www.linkedin.com/in/bemendoza/',
+    },
+    {
+      id: crypto.randomUUID(),
+      name: 'GitHub',
+      to: 'https://github.com/b-mendoza',
+    },
+    {
+      id: crypto.randomUUID(),
+      name: 'Email',
+      to: 'mailto:bmendoza.dev@gmail.com',
+    },
+  ];
+};
