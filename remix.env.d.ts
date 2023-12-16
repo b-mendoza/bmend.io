a1031772467daf3e1df9d8da8afc2769886f18a4
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-16 16:42:58 -0600

    fix(types): update Remix environment type references and declare static content manifest
    Updated the environment type references for Remix to properly reference types for `@remix-run/dev` and added references for `@remix-run/cloudflare` and `@cloudflare/workers-types` to align with the usage in a Cloudflare Workers environment. Also introduced a declaration for the `__STATIC_CONTENT_MANIFEST` module, exporting a default manifest string constant.

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,2 +1,9 @@
-import type {} from '@remix-run/dev';
-import type {} from '@vercel/remix';
+/// <reference types="@remix-run/dev" />
+/// <reference types="@remix-run/cloudflare" />
+/// <reference types="@cloudflare/workers-types" />
+
+declare module '__STATIC_CONTENT_MANIFEST' {
+  const manifest: string;
+
+  export default manifest;
+}
