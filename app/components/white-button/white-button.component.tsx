67b3353cb5cc822f26b7b1d3a1e1f47ca0df309d
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-16 01:58:08 -0600

    feat(white-button): add new WhiteButton component
    Adds a new `WhiteButton` component with customized styles. The component is exported for use elsewhere in the application, enhancing UI consistency and expediting development of features requiring a white-styled button. This component extends the generic `Button` by applying specific class names to achieve the desired white styling and hover effects.

---
+++ app/components/white-button/white-button.component.tsx
@@ -0,0 +1,20 @@
+import { Button } from '~/components/button';
+import { cn } from '~/utils/cn';
+
+type WhiteButtonProps = React.JSX.IntrinsicElements['button'];
+
+export const WhiteButton = (props: WhiteButtonProps) => {
+  const { children, className, ...restOfProps } = props;
+
+  return (
+    <Button
+      {...restOfProps}
+      className={cn(
+        'border-none bg-white text-violet-glow hover:bg-black hover:text-white',
+        className,
+      )}
+    >
+      {children}
+    </Button>
+  );
+};
