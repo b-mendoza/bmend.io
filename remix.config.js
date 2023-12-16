22b07389b18708a3eba97cc6e767da7ca71dcce9
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-16 16:52:33 -0600

    feat(remix-config): extend server configuration for Remix app
    Added additional server-related configurations to `remix.config.js` to enhance control over the application's server-side behavior. These changes include specifying a custom server file, setting conditions for different environments (workerd, worker, and browser), defining a regex pattern for bundling server dependencies while excluding the static content manifest, and improving the server build with minification and ECMAScript module format support.
    
    - The server entry point is now explicitly set to `./server.ts`.
    - A new array `serverConditions` defines the environments that the server can run in.
    - `serverDependenciesToBundle` provides a mechanism to bundle all necessary dependencies except the static content manifest.
    - `serverMainFields` prioritizes the fields used when resolving modules.
    - `serverMinify` enables minification for the server build to optimize performance.
    - `serverModuleFormat` is set to 'esm' (ECMAScript Module) for better compatibility and standardization.
    - `serverPlatform` has been set to 'neutral', accommodating various JavaScript environments.

--- remix.config.js
+++ remix.config.js
@@ -3,9 +3,19 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   cacheDirectory: './node_modules/.cache/remix',
-  ignoredRouteFiles: ['**/.*'],
   future: {
     v3_fetcherPersist: true,
     v3_relativeSplatPath: true,
   },
+  ignoredRouteFiles: ['**/.*'],
+  server: './server.ts',
+  serverConditions: ['workerd', 'worker', 'browser'],
+  serverDependenciesToBundle: [
+    // bundle everything except the virtual module for the static content manifest provided by wrangler
+    /^(?!.*\b__STATIC_CONTENT_MANIFEST\b).*$/,
+  ],
+  serverMainFields: ['browser', 'module', 'main'],
+  serverMinify: true,
+  serverModuleFormat: 'esm',
+  serverPlatform: 'neutral',
 };
