bea94051d8dd0f2bdf2818e44aa75bf312693b9f
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-16 01:57:27 -0600

    feat(link): add CustomLink component with Remix and external support
    This commit introduces a new `CustomLink` component that encapsulates the functionality for both internal links using `@remix-run/react`'s `Link` component and external links. The component automatically handles the distinction between internal and external links through a prop `isExternal`. It also includes base styling with hover effects and ensures external links have appropriate attributes for security (rel="noopener noreferrer") and behavior (target="_blank").

---
+++ app/components/link/link.component.tsx
@@ -0,0 +1,45 @@
+import { Link } from '@remix-run/react';
+import type { LinkProps } from '@remix-run/react';
+import { cn } from '~/utils/cn';
+
+const BASE_CLASS_NAMES =
+  'px-6 py-[0.4rem] text-white hover:rounded-[2rem] hover:bg-white/10';
+
+type AsRemixLink = LinkProps & {
+  isExternal?: false;
+};
+
+type AsExternalLink = React.JSX.IntrinsicElements['a'] & {
+  isExternal: true;
+};
+
+type CustomLinkProps = AsRemixLink | AsExternalLink;
+
+export const CustomLink = (props: CustomLinkProps) => {
+  const { children, className, isExternal } = props;
+
+  if (isExternal === true) {
+    const { isExternal: _isExternal, ...restOfAnchorProps } = props;
+
+    return (
+      <a
+        {...restOfAnchorProps}
+        className={cn(BASE_CLASS_NAMES, className)}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        {children}
+      </a>
+    );
+  }
+
+  const { isExternal: _isExternal, ...restOfRemixLinkProps } = props;
+
+  return (
+    <Link {...restOfRemixLinkProps} className={cn(BASE_CLASS_NAMES, className)}>
+      {children}
+    </Link>
+  );
+};
+
+export { CustomLink as Link };
