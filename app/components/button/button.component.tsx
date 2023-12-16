62cc3998626861239498bcfe88eb6c70ea27c870
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-16 01:56:31 -0600

    feat(button): enhance button component with default styles and `Paragraph` wrapper
    Added default styling to the Button component using a utility function `cn` for conditional class name concatenation, enabling the button to have rounded corners, border specifics, padding, font settings, and hover effects. Also wrapped the button's children within a `Paragraph` component to enforce consistent typography, providing flexibility to pass additional style classes through the `className` prop. This change improves UI consistency and makes the button more versatile and visually appealing.

--- app/components/button/button.component.tsx
+++ app/components/button/button.component.tsx
@@ -1,7 +1,21 @@
+import { cn } from '~/utils/cn';
+import { Paragraph } from '~/components/typography/paragraph';
+
 export type ButtonProps = React.JSX.IntrinsicElements['button'];
 
 export const Button = (props: ButtonProps) => {
-  const { type = 'button', ...restOfProps } = props;
+  const { children, className, type = 'button', ...restOfProps } = props;
 
-  return <button type={type} {...restOfProps} />;
+  return (
+    <button
+      className={cn(
+        'rounded-2xl border-[0.1rem] border-texts px-[2.2rem] py-[1.4rem] font-medium hover:bg-button-hover-background hover:text-button-hover-text',
+        className,
+      )}
+      type={type}
+      {...restOfProps}
+    >
+      <Paragraph size="lg">{children}</Paragraph>
+    </button>
+  );
 };
