8bc66d6b9f4f5bc53efce9cd3a1655c7687642f3
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-16 21:23:28 -0600

    refactor(server): migrate to Remix on Cloudflare Pages function handler
    Replaced the custom fetch request handler with the `createPagesFunctionHandler` from `@remix-run/cloudflare-pages`. This shift streamlines server.ts by using built-in support for Cloudflare Pages, assuming better integration with the platform. The new handler is responsible for managing requests and providing load context. Removed now-obsolete imports and configuration related to `getAssetFromKV` and static asset handling which are no longer required.

--- server.ts
+++ server.ts
@@ -1,62 +1,13 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
-import type { AppLoadContext } from '@remix-run/cloudflare';
-import { createRequestHandler, logDevReady } from '@remix-run/cloudflare';
+import { logDevReady } from '@remix-run/cloudflare';
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 import * as build from '@remix-run/dev/server-build';
-import __STATIC_CONTENT_MANIFEST from '__STATIC_CONTENT_MANIFEST';
-import type { Options } from '@cloudflare/kv-asset-handler';
-
-const MANIFEST = JSON.parse(
-  __STATIC_CONTENT_MANIFEST,
-) as Options['ASSET_MANIFEST'];
-
-const handleRemixRequest = createRequestHandler(build, process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === 'development') {
   logDevReady(build);
 }
 
-export default {
-  async fetch(
-    request: Request,
-    env: {
-      __STATIC_CONTENT: Fetcher;
-    },
-    ctx: ExecutionContext,
-  ): Promise<Response> {
-    try {
-      const url = new URL(request.url);
-      const ttl = url.pathname.startsWith('/build/')
-        ? 60 * 60 * 24 * 365 // 1 year
-        : 60 * 5; // 5 minutes
-
-      return await getAssetFromKV(
-        {
-          request,
-          waitUntil: ctx.waitUntil.bind(ctx),
-        } satisfies Pick<FetchEvent, 'request' | 'waitUntil'>,
-        {
-          ASSET_NAMESPACE: env.__STATIC_CONTENT,
-          ASSET_MANIFEST: MANIFEST,
-          cacheControl: {
-            browserTTL: ttl,
-            edgeTTL: ttl,
-          },
-        },
-      );
-    } catch {
-      // No-op
-    }
-
-    try {
-      const loadContext: AppLoadContext = {
-        env,
-      };
-
-      return await handleRemixRequest(request, loadContext);
-    } catch (error) {
-      console.log(error);
-
-      return new Response('An unexpected error occurred', { status: 500 });
-    }
-  },
-};
+export const onRequest = createPagesFunctionHandler({
+  build,
+  getLoadContext: (context) => ({ env: context.env }),
+  mode: build.mode,
+});
