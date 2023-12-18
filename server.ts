a21668b022aaba7f332240fb13fdab7150f83897
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-17 22:25:09 -0600

    feat(server): integrate environment schema validation with zod
    Added Zod schema validation for the environment configuration in the Remix server setup to ensure runtime type safety. The 'EnvSchema' object now validates the structure of 'env' from the Cloudflare AppLoadContext before it's passed to the getLoadContext function, ensuring only expected environment variables are present and correctly typed.

--- server.ts
+++ server.ts
@@ -1,13 +1,29 @@
-import { logDevReady } from '@remix-run/cloudflare';
+import { logDevReady, type AppLoadContext } from '@remix-run/cloudflare';
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 import * as build from '@remix-run/dev/server-build';
+import { z } from 'zod';
+
+const EnvSchema = z.object({}).passthrough();
+
+type Env = z.infer<typeof EnvSchema>;
+
+declare module '@remix-run/cloudflare' {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface AppLoadContext {
+    env: Env;
+  }
+}
 
 if (process.env.NODE_ENV === 'development') {
   logDevReady(build);
 }
 
-export const onRequest = createPagesFunctionHandler({
+export const onRequest = createPagesFunctionHandler<AppLoadContext['env']>({
   build,
-  getLoadContext: (context) => ({ env: context.env }),
+  getLoadContext: (context) => {
+    const parsedEnv = EnvSchema.parse(context.env);
+
+    return { env: parsedEnv };
+  },
   mode: build.mode,
 });
