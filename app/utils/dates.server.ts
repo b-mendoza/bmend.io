8b8c60154df3cf5d40a2c6a1ade8bee030947b27
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-18 17:54:15 -0600

    feat(utils): add date utility functions for server-side operations
    Add new utility functions to handle dates in the server-side codebase. The `utilsGetDaysInSeconds` function calculates the number of seconds in a given number of days using the dayjs library and its duration plugin.

---
+++ app/utils/dates.server.ts
@@ -0,0 +1,21 @@
+import dayjs from 'dayjs';
+import durationPlugin from 'dayjs/plugin/duration';
+
+dayjs.extend(durationPlugin);
+
+export const utilsGetDaysInSeconds = (days: number) => {
+  return dayjs.duration(days, 'days').asSeconds();
+};
+
+/**
+ * Formats a given date string to the format 'MMM YYYY'.
+ *
+ * @param {string} date - The original date string that needs to be formatted.
+ * @returns {string} - Returns the formatted date string according to the specified format.
+ *
+ * Example Input/Output:
+ * formatDate('2023-04-01') will return 'Apr 2023'
+ */
+export const formatDate = (date: string) => {
+  return dayjs(date).format('MMM YYYY');
+};
