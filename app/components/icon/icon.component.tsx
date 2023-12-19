9e9bf58abe79abd07b8879dfd48cba3f4bd312f7
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-19 14:21:21 -0600

    feat(icons): implement svg icons with types and helper component
    Added several SVG icons as individual symbols within a single SVG using `<symbol>` tags, each with a unique id. Created an `Icon` component in React that accepts a `name` prop (corresponding to the symbol id) and spreads the rest of its props onto the rendered `svg` element. Also implemented type safety by defining the `IconName` type, which includes all possible icon names.
    
    The `Icon` component utilizes `<use>` to reference the appropriate symbol based on the `name` prop passed, effectively reusing SVG content for efficient icon rendering.
    
    To make sure all icons are available from a central point, exported everything from the newly created `icon.component.tsx` file through an `index.ts` file.

---
+++ app/components/icon/icon.component.tsx
@@ -0,0 +1,45 @@
+import href from './icon.svg';
+
+export { href };
+
+type IconProps = React.JSX.IntrinsicElements['svg'] & {
+  readonly name: string;
+};
+
+export const Icon = (props: IconProps) => {
+  const { name, ...restOfProps } = props;
+
+  return (
+    <svg {...restOfProps}>
+      <use href={`${href}#${name}`} />
+    </svg>
+  );
+};
+
+const ICON_NAMES = [
+  'bars-3',
+  'css',
+  'gatsby',
+  'git',
+  'graphql',
+  'html5',
+  'javascript',
+  'jest',
+  'mysql',
+  'nextjs',
+  'nodejs',
+  'postgresql',
+  'prisma_dark',
+  'react',
+  'redux',
+  'remix',
+  'sass',
+  'sqlite',
+  'storybook',
+  'tailwindcss',
+  'testing-library',
+  'typescript',
+  'x-mark',
+] as const;
+
+export type IconName = (typeof ICON_NAMES)[number];
