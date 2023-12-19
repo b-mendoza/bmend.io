6176003ed79644af31c6404b1dd691d9617be38f
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-19 17:12:34 -0600

    feat(tags): add icons to tags and expand tag list
    Added an 'icon' field to the Tag type definition and updated getTags function to return an extended list of technology-related tags with associated icons. The function now also uses a type assertion to ensure the returned array satisfies the Tag[] type.

--- app/routes/_index/get-tags.server.ts
+++ app/routes/_index/get-tags.server.ts
@@ -1,38 +1,108 @@
+import type { IconName } from '~/components/icon';
+
 export type Tag = {
   id: string;
   text: string;
+  icon?: IconName;
 };
 
-export const getTags = (): Tag[] => {
+export const getTags = () => {
   // This might be replaced by a database/CMS call in the future
   return [
     {
       id: crypto.randomUUID(),
-      text: 'UI/UX Design',
+      text: 'Git',
+      icon: 'git',
     },
     {
       id: crypto.randomUUID(),
-      text: 'Interaction Design',
+      text: 'HTML',
+      icon: 'html5',
     },
     {
       id: crypto.randomUUID(),
-      text: 'Brand Identity',
+      text: 'CSS',
+      icon: 'css3',
     },
     {
       id: crypto.randomUUID(),
-      text: 'Web Design',
+      text: 'SASS',
+      icon: 'sass',
     },
     {
       id: crypto.randomUUID(),
-      text: 'Product Design',
+      text: 'JavaScript',
+      icon: 'javascript',
     },
     {
       id: crypto.randomUUID(),
-      text: 'Typography',
+      text: 'TypeScript',
+      icon: 'typescript',
     },
     {
       id: crypto.randomUUID(),
-      text: 'App User Interface',
+      text: 'React',
+      icon: 'react',
     },
-  ];
+    {
+      id: crypto.randomUUID(),
+      text: 'Redux',
+      icon: 'redux',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Next.js',
+      icon: 'nextdotjs',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Gatsby',
+      icon: 'gatsby',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Remix',
+      icon: 'remix',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'GraphQL',
+      icon: 'graphql',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Jest',
+      icon: 'jest',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'RTL',
+      icon: 'testinglibrary',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Node.js',
+      icon: 'nodedotjs',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'PostgreSQL',
+      icon: 'postgresql',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'tailwindcss',
+      icon: 'tailwindcss',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Storybook',
+      icon: 'storybook',
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Prisma',
+      icon: 'prisma',
+    },
+  ] satisfies Tag[];
 };
