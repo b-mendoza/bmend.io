7714cb519467e21e0298417447a0265b582e65e8
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-19 11:44:17 -0600

    feat(utils): update job experience utility with dynamic date formatting and new entries
    Updated the `getJobExperience` utility to dynamically format dates using a new helper function, `joinDates`, and incorporated `formatDate` for consistency. Added new job experience entries to reflect updated work history, introduced multi-line descriptions for roles, and used template literals for better readability.

--- app/utils/get-job-experience.server.ts
+++ app/utils/get-job-experience.server.ts
@@ -1,28 +1,40 @@
+import { formatDate } from '~/utils/dates.server';
+
+const joinDates = (dates: [string, string]) => {
+  return dates.join(' - ');
+};
+
 export const getJobExperience = () => {
   return [
     {
       id: crypto.randomUUID(),
-      companyName: 'InnovateTech Solutions',
+      companyName: 'Praxent',
       description:
-        'Lead the UX design team in developing innovative user experiences for tech-driven products. Collaborate closely with cross-functional teams to implement user-centered design principles and drive product success.',
-      jobTitle: 'Senior UX Architect',
-      timePeriod: '2019 - now',
+        'Leading the development of BaaS (Banking as a Service) platforms in the Fintech industry, using cutting-edge tools and frameworks for backend and frontend systems',
+      jobTitle: 'Senior Software Engineer',
+      timePeriod: joinDates([formatDate('2022-07'), 'Present']),
     },
     {
       id: crypto.randomUUID(),
-      companyName: 'NexTech Innovations',
-      description:
-        'Played a key role in designing intuitive user interfaces and enhancing user experiences for cutting-edge technology solutions. Conducted user research and usability testing to inform design decisions.',
-      jobTitle: 'UX Designer',
-      timePeriod: '2016 - 2019',
+      companyName: 'Bejamas',
+      description: `• Collaborated with stakeholders to define project requirements, while integrating headless CMS tools like Contentful and Storyblok\n\n• Employed and evaluated cutting-edge web standards, such as Core Web Vitals, to optimize website performance and improve business conversion rates\n\n• Supported the development of internal company standards by authoring comprehensive documentation on modern web practices and tools for fellow developers`,
+      jobTitle: 'Frontend Developer',
+      timePeriod: joinDates([formatDate('2022-02'), formatDate('2022-07')]),
     },
     {
       id: crypto.randomUUID(),
-      companyName: 'Digital Vision Corporation',
+      companyName: 'NBC Universal',
       description:
-        'Gained foundational design experience by supporting the design team in various projects. Assisted in creating wireframes, prototypes, and visual assets, contributing to the development of user-centered designs.',
-      jobTitle: 'Design Intern',
-      timePeriod: '2015 - 2016',
+        'Implemented headless CMS solutions utilizing WordPress and Strapi, seamlessly integrating them with Identity services for enhanced security while also defining API requirements and collaborated with tools like Webpack and Babel to deliver optimized and efficient production builds.',
+      jobTitle: 'Senior Frontend Developer',
+      timePeriod: joinDates([formatDate('2021-02'), formatDate('2022-11')]),
+    },
+    {
+      id: crypto.randomUUID(),
+      companyName: 'Applaudo Studios',
+      description: `• Contributed to the development of multi-tenant platforms and frontends that featured white labeling and dynamic feature toggles\n\n• Optimized web components for maximum performance across various devices and browsers, ensuring a seamless user experience\n\n• Translated designs and wireframes into high-quality code, resulting in pixel-perfect user interfaces\n\n• Employed GraphQL and RESTful APIs to streamline data processing and communication with backend systems`,
+      jobTitle: 'Frontend Developer',
+      timePeriod: joinDates([formatDate('2019-02'), formatDate('2022-05')]),
     },
   ];
 };
