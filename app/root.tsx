108f670f82a70e5889239750d4b3f94b342e2c51
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-18 19:45:29 -0600

    feat(root): refactor homepage with new layout and data integration
    Refactored the home layout of the application by integrating the loader, meta, headers, and UI components directly into `root.tsx` instead of having a separate route file `_index/route.tsx`. Moved utility functions to a common utils directory for better organization and reuse. Added new components to enhance the visual structure and data presentation on the home page.
    
    The loader now fetches job experience, social links, and tags directly within `root.tsx`, which are then mapped and rendered using the `ExperienceCard`, `TagMapper`, and other UI components. Additionally, cache-control policies have been defined in the headers function to optimize content delivery. The `MetaFunction` was uncommented, setting the title of the portfolio page explicitly.
    
    Deleted the previous `_index/route.tsx` as its functionalities have been merged into `root.tsx`. Renamed utility server files for consistency and moved them from the routes directory to the utils directory to signify their shared nature across components.

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,34 @@
-import type { LinksFunction } from '@remix-run/cloudflare';
+import type { OutgoingHttpHeaders } from 'http';
+import type {
+  HeadersFunction,
+  LinksFunction,
+  MetaFunction,
+} from '@remix-run/cloudflare';
+import { json } from '@remix-run/cloudflare';
 import {
   Links,
   LiveReload,
   Meta,
-  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react';
+import { Image } from '@unpic/react';
 import geistFont from '~/assets/fonts/GeistVariableVF.woff2';
+import { ExperienceCard } from '~/components/experience-card';
+import { Link } from '~/components/link';
+import { TagMapper } from '~/components/tag-mapper';
+import { Heading } from '~/components/typography/headings';
+import { Paragraph } from '~/components/typography/paragraph';
+import { Subtitle } from '~/components/typography/subtitle';
+import { SectionWrapper } from '~/components/ui/section-wrapper';
+import { WhiteButton } from '~/components/white-button';
 import fontStyles from '~/styles/fonts.styles.css';
 import globalStyles from '~/styles/globals.styles.css';
+import { getDaysInSeconds } from '~/utils/get-days-in-seconds.server';
+import { getJobExperience } from '~/utils/get-job-experience.server';
+import { getSocialLinks } from '~/utils/get-social-links.server';
+import { getTags } from '~/utils/get-tags.server';
 
 export const links: LinksFunction = () => {
   return [
@@ -41,7 +60,40 @@ export const links: LinksFunction = () => {
   ];
 };
 
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Bryan M. | Portfolio' },
+    // { name: 'description', content: 'Welcome to Remix!' },
+  ];
+};
+
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+  const loaderCacheControlPolicy = loaderHeaders.get('cache-control');
+
+  if (typeof loaderCacheControlPolicy === 'string') {
+    return {
+      'cache-control': loaderCacheControlPolicy,
+    };
+  }
+
+  return {
+    'cache-control': `max-age=${getDaysInSeconds(
+      1,
+    )}, stale-while-revalidate=${getDaysInSeconds(7)}`,
+  } satisfies OutgoingHttpHeaders;
+};
+
+export const loader = () => {
+  return json({
+    jobExperience: getJobExperience(),
+    socialLinks: getSocialLinks(),
+    tags: getTags(),
+  });
+};
+
 export default function HomeLayoutRoute() {
+  const loaderData = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -50,8 +102,82 @@ export default function HomeLayoutRoute() {
         <Meta />
         <Links />
       </head>
-      <body className="min-h-screen bg-background p-4">
-        <Outlet />
+      <body className="min-h-screen bg-background p-4 text-white">
+        <div className="flex flex-col gap-4 text-white">
+          <SectionWrapper>
+            <Subtitle className="mb-12">About</Subtitle>
+
+            <Heading className="mb-8 font-medium" size="sm" variant="h1">
+              User Experience Architect & Design Innovator
+            </Heading>
+
+            <Paragraph className="mb-16">
+              In my 8 years of experience, I have completed 50 successful
+              projects. I embrace a holistic approach to design thinking,
+              specializing in precise implementation
+            </Paragraph>
+
+            <TagMapper
+              getTagId={(tag) => tag.id}
+              getTagName={(tag) => tag.text}
+              tags={loaderData.tags}
+            />
+          </SectionWrapper>
+
+          <SectionWrapper className="flex flex-col gap-[3.6rem]">
+            <Subtitle>Experience</Subtitle>
+
+            {loaderData.jobExperience.map((jobExperience) => (
+              <ExperienceCard
+                companyName={jobExperience.companyName}
+                description={jobExperience.description}
+                jobTitle={jobExperience.jobTitle}
+                key={jobExperience.id}
+                timePeriod={jobExperience.timePeriod}
+              />
+            ))}
+          </SectionWrapper>
+
+          <SectionWrapper className="border-white/20 bg-gradient-to-br from-[hsl(243_100%_68%)] to-[hsl(243_76%_51%)] px-0">
+            <Image
+              alt="a close up of a dog on a leash with its mouth open and tongue out and tongue hanging out"
+              background="https://res.cloudinary.com/dgqif0kkr/image/upload/c_fit,h_115,w_115/q_1/e_blur:2000/f_webp/bmendoza-io/sm8a1hhpi7wjua4upya1.jpg"
+              cdn="cloudinary"
+              className="mx-auto mb-10 rounded-full"
+              height={115}
+              layout="fixed"
+              src="https://res.cloudinary.com/dgqif0kkr/image/upload/q_auto:best/c_fit,h_115,w_115/f_webp/bmendoza-io/sm8a1hhpi7wjua4upya1.jpg"
+              width={115}
+            />
+
+            <Paragraph className="mb-6 text-center text-white/80">
+              John Cameron
+            </Paragraph>
+
+            <div className="mx-8 mb-12">
+              <Heading
+                className="mb-12 text-center font-medium"
+                size="lg"
+                variant="h2"
+              >
+                Let&apos;s talk about your project
+              </Heading>
+
+              <WhiteButton className="w-full">Book a call</WhiteButton>
+            </div>
+
+            <ul className="flex flex-wrap items-center justify-center gap-12 border-t-[0.1rem] border-t-texts/[0.15] px-8 pt-12">
+              {loaderData.socialLinks.map((socialLink) => (
+                <li key={socialLink.id}>
+                  <Link isExternal href={socialLink.to}>
+                    {socialLink.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </SectionWrapper>
+        </div>
+
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
