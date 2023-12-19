7b68431ae682490e410f43404504fda2a9f3ec68
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-19 17:10:55 -0600

    feat(tag-mapper): support displaying icons in tags
    This commit introduces the ability to display icons alongside tag names within the `TagMapper` and `Tag` components. An `IconName` import statement was added, and the `getIconName` optional function prop was included in `TagMapperProps` to supply icon names for each tag. The `Tag` component now accepts an `icon` prop of type `IconName` and conditionally renders an `Icon` component if an icon name is provided.

--- app/components/tag-mapper/tag-mapper.component.tsx
+++ app/components/tag-mapper/tag-mapper.component.tsx
@@ -1,3 +1,4 @@
+import type { IconName } from '~/components/icon';
 import { Tag } from '~/components/tag';
 import { cn } from '~/utils/cn';
 
@@ -5,23 +6,33 @@ type TagMapperProps<T> = Readonly<{
   getTagId: (tag: T) => string;
   getTagName: (tag: T) => string;
   tags: T[];
+  getIconName?: (tag: T) => IconName | undefined;
   tagClassName?: string;
   tagsWrapperClassName?: string;
 }>;
 
 export const TagMapper = <Tag,>(props: TagMapperProps<Tag>) => {
-  const { getTagId, getTagName, tags, tagClassName, tagsWrapperClassName } =
-    props;
+  const {
+    getTagId,
+    getTagName,
+    tags,
+    getIconName,
+    tagClassName,
+    tagsWrapperClassName,
+  } = props;
 
   return (
     <ul className={cn('flex flex-wrap gap-[0.8rem]', tagsWrapperClassName)}>
-      {tags.map((tag) => (
-        <Tag
-          className={tagClassName}
-          key={getTagId(tag)}
-          name={getTagName(tag)}
-        />
-      ))}
+      {tags.map((tag) => {
+        return (
+          <Tag
+            className={tagClassName}
+            key={getTagId(tag)}
+            name={getTagName(tag)}
+            icon={getIconName?.(tag)}
+          />
+        );
+      })}
     </ul>
   );
 };
--- app/components/tag/tag.component.tsx
+++ app/components/tag/tag.component.tsx
@@ -1,21 +1,29 @@
+import type { IconName } from '~/components/icon';
+import { Icon } from '~/components/icon';
 import { Paragraph } from '~/components/typography/paragraph';
 import { cn } from '~/utils/cn';
 
-type TagProps = Pick<React.JSX.IntrinsicElements['li'], 'className'> & {
-  readonly name: string;
-};
+type TagProps = Pick<React.JSX.IntrinsicElements['li'], 'className'> &
+  Readonly<{
+    name: string;
+    icon?: IconName;
+  }>;
 
 export const Tag = (props: TagProps) => {
-  const { name, className } = props;
+  const { name, className, icon: iconName } = props;
 
   return (
     <li
       className={cn(
-        'rounded-[2rem] bg-block-background-1/[0.15] px-4 py-[0.4rem]',
+        'flex items-center gap-2 rounded-[2rem] bg-block-background-1/[0.15] px-4 py-[0.4rem]',
         className,
       )}
     >
       <Paragraph size="sm">{name}</Paragraph>
+
+      {typeof iconName === 'string' ? (
+        <Icon className="fill-white" name={iconName} height={20} width={20} />
+      ) : null}
     </li>
   );
 };
