306da2b31d6d6cd285feaf58e07927d35a5c9c48
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-19 00:42:10 -0600

    fix(button): use PARAGRAPH_SIZES constant for button text size
    Replaced the direct import of the Paragraph component with the PARAGRAPH_SIZES constant to set the text size within the Button component. This change ensures consistent typography sizes and reduces direct component dependencies, aligning with the approach of using style constants for design uniformity. The button's children are now rendered directly as its content instead of being wrapped in a Paragraph component, simplifying the DOM structure and potentially improving performance.

--- app/components/button/button.component.tsx
+++ app/components/button/button.component.tsx
@@ -1,4 +1,4 @@
-import { Paragraph } from '~/components/typography/paragraph';
+import { PARAGRAPH_SIZES } from '~/components/typography/paragraph';
 import { cn } from '~/utils/cn';
 
 export type ButtonProps = React.JSX.IntrinsicElements['button'];
@@ -10,12 +10,13 @@ export const Button = (props: ButtonProps) => {
     <button
       className={cn(
         'rounded-2xl border-[0.1rem] border-texts px-[2.2rem] py-[1.4rem] font-medium hover:bg-button-hover-background hover:text-button-hover-text',
+        PARAGRAPH_SIZES.lg,
         className,
       )}
       type={type}
       {...restOfProps}
     >
-      <Paragraph size="lg">{children}</Paragraph>
+      {children}
     </button>
   );
 };
