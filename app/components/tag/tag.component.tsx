e37d45005809ca995020baeafaefd3f6afa5e2a6
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-20 20:56:16 -0600

    feat(tag-component): support custom icon sizes in Tag component

--- app/components/tag/tag.component.tsx
+++ app/components/tag/tag.component.tsx
@@ -1,7 +1,12 @@
-import type { IconName } from '~/components/icon';
+import type { IconName, IconSize } from '~/components/icon';
 import { Icon } from '~/components/icon';
 import { Paragraph } from '~/components/typography/paragraph';
 import { cn } from '~/utils/cn';
+
+const DEFAULT_ICON_SIZE: IconSize = {
+  height: 20,
+  width: 20,
+};
 
 type TagProps = Pick<React.JSX.IntrinsicElements['li'], 'className'> &
   Readonly<{
@@ -9,20 +14,51 @@ type TagProps = Pick<React.JSX.IntrinsicElements['li'], 'className'> &
     icon?: IconName;
   }>;
 
+const CUSTOM_ICON_SIZES = {
+  javascript: {
+    height: 18,
+    width: 18,
+  },
+  remix: {
+    height: 16,
+    width: 16,
+  },
+  tailwindcss: {
+    height: 22,
+    width: 22,
+  },
+  typescript: {
+    height: 18,
+    width: 18,
+  },
+} satisfies Partial<Record<IconName, IconSize>>;
+
+const hasCustomSize = (
+  iconName: IconName | undefined,
+): iconName is keyof typeof CUSTOM_ICON_SIZES => {
+  if (iconName == null) return false;
+
+  return iconName in CUSTOM_ICON_SIZES;
+};
+
 export const Tag = (props: TagProps) => {
   const { name, className, icon: iconName } = props;
+
+  const iconSize = hasCustomSize(iconName)
+    ? CUSTOM_ICON_SIZES[iconName]
+    : DEFAULT_ICON_SIZE;
 
   return (
     <li
       className={cn(
-        'flex items-center gap-2 rounded-[2rem] bg-block-background-1/[0.15] px-4 py-[0.4rem]',
+        'flex h-12 items-center gap-2 rounded-[2rem] bg-block-background-1/[0.15] px-4 py-[0.4rem]',
         className,
       )}
     >
       <Paragraph size="sm">{name}</Paragraph>
 
       {typeof iconName === 'string' ? (
-        <Icon className="fill-white" name={iconName} height={20} width={20} />
+        <Icon className="fill-white" name={iconName} {...iconSize} />
       ) : null}
     </li>
   );
