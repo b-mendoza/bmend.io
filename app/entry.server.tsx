212f1849e2bcccc3bfc0951c5d785f4ffed04e44
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-22 14:05:21 -0600

    feat(server-rendering): implement Remix server entry

---
+++ app/entry.server.tsx
@@ -0,0 +1,36 @@
+import type { EntryContext } from '@remix-run/cloudflare';
+import { RemixServer } from '@remix-run/react';
+import { isbot } from 'isbot';
+import { renderToReadableStream } from 'react-dom/server';
+
+export default async function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
+  const body = await renderToReadableStream(
+    <RemixServer context={remixContext} url={request.url} />,
+    {
+      signal: request.signal,
+      onError(error) {
+        // Log streaming rendering errors from inside the shell
+        console.error(error);
+        responseStatusCode = 500;
+      },
+    },
+  );
+
+  const userAgentHeader = request.headers.get('user-agent');
+
+  if (typeof userAgentHeader === 'string' && isbot(userAgentHeader)) {
+    await body.allReady;
+  }
+
+  responseHeaders.set('Content-Type', 'text/html');
+
+  return new Response(body, {
+    headers: responseHeaders,
+    status: responseStatusCode,
+  });
+}
