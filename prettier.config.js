aa12b92537957e5c5b2f8dc726b52fbfdb0e6c9c
Bryan Mendoza <bmendoza.dev@hotmail.com>
2024-01-09 15:14:37 -0600

    refactor: remove `prettier-plugin-packagejson` Prettier plugin

--- prettier.config.js
+++ prettier.config.js
@@ -3,7 +3,7 @@
 /** @type {import('prettier').Config} */
 const prettierConfig = {
   singleQuote: true,
-  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-tailwindcss'],
+  plugins: ['prettier-plugin-tailwindcss'],
   tailwindFunctions: ['cn'],
 };
 
